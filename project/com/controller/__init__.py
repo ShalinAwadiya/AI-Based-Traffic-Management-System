fd48abbbe5f2f8da0467309935fa90fed4887ebe
Shalin Hasanbhai Awadiya <awadiyashalin03@gmail.com>
2022-08-27 23:49:46 -0300

    Project base structure created

---
+++ project/com/controller/__init__.py
@@ -0,0 +1,14 @@
+
+
+
+
+import project.com.controller.AreaController
+import project.com.controller.CrossroadController
+import project.com.controller.CameraController
+import project.com.controller.DatasetController
+import project.com.controller.LoginController
+import project.com.controller.PoliceStationController
+import project.com.controller.ComplainController
+import project.com.controller.FeedbackController
+import project.com.controller.VideoController
+import project.com.controller.MainController
